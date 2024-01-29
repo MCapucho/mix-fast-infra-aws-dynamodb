4bed130d7bc8a132cab388bed093aef6abd1a218
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-01-28 21:02:49 -0300

    Implementação da Infra DynamoDB

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,12 +1,4 @@
 resource "aws_dynamodb_table" "mixfast_dynamodb" {
-  name           = "mixfastproducao"
-  hash_key       = "codigoPedido"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-}
-
-resource "aws_dynamodb_table" "mixfast_dynamodb" {
   name           = var.name
   read_capacity  = 5
   write_capacity = 5
